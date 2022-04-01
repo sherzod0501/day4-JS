79348c890b0c77166f630efd5cbaad0fe7e7d8d1
sher0501 <asoggamer@gmail.com>
2022-04-01 14:20:36 +0500

    day4-js

---
+++ Js/index.js
@@ -0,0 +1,38 @@
+let x = 9;
+console.log(x);
+
+let sh = 3;
+console.log(sh);
+
+let e = 2; 
+console.log(e);
+
+let r = 1; 
+console.log(r);
+
+let z = 4;
+console.log(z);
+
+let o = 5;
+console.log(o);
+
+let d = 6;
+console.log (d);
+
+let s = 20;
+let S = 27;
+
+console.log (s == S);
+
+console.timeEnd ("biron nima");
+console.error ("xatolik");
+console.info ("informatsiya");
+
+alert ("malumotlar");
+
+
+let number = 20 + 15;
+console.log (number) ;
+
+
+
